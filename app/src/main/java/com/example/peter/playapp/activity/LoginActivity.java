b2a287d0080e355af44af017a85c138d4927a46c
Peter_Fu <fuhuicheng@esunny.cc>
2018-05-09 17:29:50 +0800

    1.  update

--- app/src/main/java/com/example/peter/playapp/activity/LoginActivity.java
+++ app/src/main/java/com/example/peter/playapp/activity/LoginActivity.java
@@ -2,20 +2,34 @@ package com.example.peter.playapp.activity;
 
 import android.support.annotation.NonNull;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.peter.playapp.R;
 import com.example.peter.playapp.base.BaseActivity;
 import com.example.peter.playapp.base.MvpActivity;
+import com.example.peter.playapp.bean.UserInfo;
 import com.example.peter.playapp.mvp.contract.LoginContract;
 import com.example.peter.playapp.mvp.model.LoginModel;
 import com.example.peter.playapp.mvp.presenter.LoginPresenter;
 import com.example.peter.playapp.mvp.presenter.MainPresenter;
 import com.example.peter.playapp.mvp.view.LoginView;
+import com.google.gson.Gson;
+
+import butterknife.BindString;
+import butterknife.BindView;
+import butterknife.OnClick;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
 public class LoginActivity extends MvpActivity<LoginPresenter> implements LoginView{
 
+    @BindView(R.id.activity_login_et_account)
+    EditText et_account;
+    @BindView(R.id.activity_login_et_password)
+    EditText et_password;
+    @BindView(R.id.activity_login_tv_login)
+    Button btn_login;
 
     @Override
     public void initData() {
@@ -30,6 +44,12 @@ public class LoginActivity extends MvpActivity<LoginPresenter> implements LoginV
     @Override
     public void onClick(View v) {
 
+    }
+
+    @OnClick(R.id.activity_login_tv_login)
+    public void setBtn_login(){
+        UserInfo userInfo = new UserInfo();
+//        mvpPresenter.login();
     }
 
     @Override
