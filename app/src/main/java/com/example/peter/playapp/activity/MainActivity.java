a4cb61c5a84869eb6ff3ba66c47e6a86bb46f1c4
Peter_Fu <fuhuicheng@esunny.cc>
2018-08-14 13:22:45 +0800

    1. Add the RxJava code.

--- app/src/main/java/com/example/peter/playapp/activity/MainActivity.java
+++ app/src/main/java/com/example/peter/playapp/activity/MainActivity.java
@@ -4,9 +4,11 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Environment;
 import android.os.Parcel;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -20,19 +22,44 @@ import com.example.peter.playapp.bean.UserInfo;
 import com.example.peter.playapp.util.FixDexUtils;
 import com.example.peter.playapp.util.SimpleHotFixBugTest;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
 import butterknife.BindView;
 import butterknife.OnClick;
+import io.reactivex.Flowable;
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.ObservableSource;
 import io.reactivex.Observer;
+import io.reactivex.Single;
+import io.reactivex.SingleObserver;
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.BiFunction;
+import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
+import io.reactivex.functions.Predicate;
+import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends BaseActivity {
 
-    @BindView(R.id.btn_fix)
-    Button btn_fix;
+//    @BindView(R.id.btn_fix)
+//    Button btn_fix;
     @BindView(R.id.btn_hot_fix)
     Button btn_hot_fix;
-    @BindView(R.id.iv_test)
-    ImageView iv_test;
+//    @BindView(R.id.iv_test)
+//    ImageView iv_test;
+
+    final String TAG = "rxJava";
+
+    @Override
+    public void initView() {
+
+    }
 
     @Override
     public void initData() {
@@ -45,34 +72,202 @@ public class MainActivity extends BaseActivity {
             };
             ActivityCompat.requestPermissions(this, PERMISSIONS_STORAGE, REQUEST_EXTERNAL_STORAGE);
         }
+
+//        learnRxJava2();
+//        learnRxJava2Map();
+//        learnRxJava2Zip();
+//        learnRxJava2Concat();
+//        learnRxJava2FlatMap();
+//        learnRxJava2Distinct();
+//        learnRxJava2Filter();
+//        learnRxJava2Buffer();
+//        learnRxJava2Timer();
+//        learnRxJava2Skip();
+//        learnRxJava2Take();
+        learnRxJava2Single();
     }
 
+    private void learnRxJava2Single() {
+        Single.just(new Random().nextInt())
+                .subscribe(new SingleObserver<Integer>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
 
-    @Override
-    public void initView() {
+                    }
 
+                    @Override
+                    public void onSuccess(Integer value) {
+                        Log.e(TAG, "single : onSuccess : "+value+"\n" );
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.e(TAG, "single : onError : "+e.getMessage()+"\n");
+                    }
+                });
     }
 
-    @Override
-    public void onClick(View v) {
-
+    private void learnRxJava2Skip() {
+        Observable.just(1, 2, 3, 4, 5)
+                .skip(2)
+                .subscribe(new Consumer<Integer>() {
+                    @Override
+                    public void accept(Integer integer) throws Exception {
+                        Log.i(TAG, "skip : " + integer + "\n");
+                    }
+                });
     }
 
-    @OnClick(R.id.btn_fix)
-    public void fix(){
-        FixDexUtils.loadFixedDex(this, Environment.getExternalStorageDirectory());
+    private void learnRxJava2Take() {
+        Flowable.fromArray(1, 2, 3, 4, 5)
+                .take(2)
+                .subscribe(new Consumer<Integer>() {
+                    @Override
+                    public void accept(Integer integer) throws Exception {
+                        Log.e(TAG, "accept: take : " + integer + "\n");
+                    }
+                });
     }
 
-    @OnClick(R.id.btn_hot_fix)
-    public void hotFix(){
-//        Toast.makeText(this, "修复之前", Toast.LENGTH_SHORT).show();
-        SimpleHotFixBugTest test = new SimpleHotFixBugTest();
-        test.getBug(this);
-//        test.changePhoto(this, iv_test);
+    private void learnRxJava2Buffer() {
+        Observable.just(1, 2, 3, 4, 5)
+                .buffer(3, 2)
+                .subscribe(new Consumer<List<Integer>>() {
+                    @Override
+                    public void accept(List<Integer> integers) throws Exception {
+                        Log.i(TAG, "buffer size : " + integers.size());
+                        Log.i(TAG, "buffer value : ");
+                        for (Integer i : integers){
+                            Log.i(TAG, i + " ");
+                        }
+                        Log.i(TAG, "\n");
+                    }
+                });
+    }
+
+    private void learnRxJava2Filter() {
+        Observable.just(1, 20, 56, -5, 7, 19)
+                .filter(new Predicate<Integer>() {
+                    @Override
+                    public boolean test(Integer integer) throws Exception {
+                        return integer >= 10;
+                    }
+                }).subscribe(new Consumer<Integer>() {
+            @Override
+            public void accept(Integer integer) throws Exception {
+                Log.i(TAG, "filter : " + integer + "\n");
+            }
+        });
+    }
+
+    private void learnRxJava2Distinct() {
+        Observable.just(1, 1, 1, 2, 2, 3, 4, 5)
+                .distinct()
+                .subscribe(new Consumer<Integer>() {
+                    @Override
+                    public void accept(Integer integer) throws Exception {
+                        Log.i(TAG, "dinstinct : " + integer + "\n");
+                    }
+                });
+    }
+
+    private void learnRxJava2FlatMap() {
+        Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> e) throws Exception {
+                e.onNext(1);
+                e.onNext(2);
+                e.onNext(3);
+            }
+        }).flatMap(new Function<Integer, ObservableSource<String>>() {
+            @Override
+            public ObservableSource<String> apply(Integer integer) throws Exception {
+                List<String> list = new ArrayList<>();
+
+                for (int i = 0; i <3; i++){
+                    list.add("I am value " + integer);
+                }
+                int delayTime = (int) (1 + Math.random() * 10);
+                return Observable.fromIterable(list).delay(delayTime, TimeUnit.MILLISECONDS);
+            }
+        }).subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<String>(){
+                    @Override
+                    public void accept(String s) throws Exception {
+                        Log.e(TAG, "flatMap : accept : " + s + "\n");
+                    }
+                });
+    }
+
+    private void learnRxJava2Concat() {
+        Observable.concat(Observable.just(1, 2, 3), Observable.just(4, 5, 6))
+                .subscribe(new Consumer<Integer>() {
+                    @Override
+                    public void accept(Integer integer) throws Exception {
+                        Log.i(TAG, "concat : " + integer + "\n");
+                    }
+                });
     }
 
     @Override
     public int getLayoutId() {
         return R.layout.activity_main;
     }
+
+    private void learnRxJava2Zip() {
+        Observable.zip(getStringObservable(), getIntegerObservable(), new BiFunction<String, Integer, String>() {
+            @Override
+            public String apply(String s, Integer integer) throws Exception {
+                return s + integer;
+            }
+        }).subscribe(new Consumer<String>() {
+            @Override
+            public void accept(String s) throws Exception {
+                Log.e(TAG, "zip : accept : " + s + "\n");
+            }
+        });
+    }
+
+    private Observable<String> getStringObservable() {
+        return Observable.create(new ObservableOnSubscribe<String>() {
+            @Override
+            public void subscribe(@NonNull ObservableEmitter<String> e) throws Exception {
+                if (!e.isDisposed()) {
+                    e.onNext("A");
+                    Log.e(TAG, "String emit : A \n");
+                    e.onNext("B");
+                    Log.e(TAG, "String emit : B \n");
+                    e.onNext("C");
+                    Log.e(TAG, "String emit : C \n");
+                }
+            }
+        });
+    }
+
+    private Observable<Integer> getIntegerObservable() {
+        return Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(@NonNull ObservableEmitter<Integer> e) throws Exception {
+                if (!e.isDisposed()) {
+                    e.onNext(1);
+                    Log.e(TAG, "Integer emit : 1 \n");
+                    e.onNext(2);
+                    Log.e(TAG, "Integer emit : 2 \n");
+                    e.onNext(3);
+                    Log.e(TAG, "Integer emit : 3 \n");
+                    e.onNext(4);
+                    Log.e(TAG, "Integer emit : 4 \n");
+                    e.onNext(5);
+                    Log.e(TAG, "Integer emit : 5 \n");
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
 }
+
--- app/src/main/java/com/example/peter/playapp/practice/keepAlive/KeepAliveActivity.java
+++ app/src/main/java/com/example/peter/playapp/practice/keepAlive/KeepAliveActivity.java
@@ -1,12 +1,24 @@
 package com.example.peter.playapp.practice.keepAlive;
 
+import android.annotation.SuppressLint;
 import android.content.BroadcastReceiver;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.graphics.PixelFormat;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.LruCache;
+import android.view.Gravity;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewManager;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 
 import com.example.peter.playapp.R;
 import com.example.peter.playapp.practice.keepAlive.jobservice.DaemonService;
@@ -22,7 +34,11 @@ public class KeepAliveActivity extends AppCompatActivity {
     Button btn_service;
     Button btn_one_px;
     Button btn_job_service;
+    Button mFloatingButton;
 
+    WindowManager.LayoutParams mLayoutParams;
+    WindowManager mWindowManager;
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -57,6 +73,41 @@ public class KeepAliveActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(KeepAliveActivity.this, DaemonService.class);
                 startService(intent);
+            }
+        });
+
+        getMainLooper().quit();
+        Looper.prepareMainLooper();
+
+        mFloatingButton = new Button(this);
+        mFloatingButton.setText("会移动");
+        mLayoutParams = new WindowManager.LayoutParams(WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.WRAP_CONTENT, 0, 0, PixelFormat.TRANSPARENT);
+        mLayoutParams.flags = WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
+                | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+                | WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED;
+        mLayoutParams.gravity = Gravity.LEFT | Gravity.TOP;
+        mLayoutParams.x = 100;
+        mLayoutParams.y = 300;
+        mLayoutParams.type = WindowManager.LayoutParams.TYPE_SYSTEM_ERROR;
+        mWindowManager = getWindowManager();
+        mWindowManager.addView(mFloatingButton, mLayoutParams);
+
+        Window window = getWindow();
+        mFloatingButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                int rawX = (int) event.getRawX();
+                int rawY = (int) event.getRawY();
+                switch (event.getAction()){
+                    case MotionEvent.ACTION_MOVE:{
+                        mLayoutParams.x = rawX;
+                        mLayoutParams.y = rawY;
+                        mWindowManager.updateViewLayout(mFloatingButton, mLayoutParams);
+                     break;
+                    }
+                }
+                return false;
             }
         });
     }
